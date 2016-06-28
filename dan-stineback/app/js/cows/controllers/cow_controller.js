4c78e31e68bbc10c6e0d35f13edcdb529099f0c3
Dan Stineback <dstineback@gmail.com>
2016-06-27 20:20:08 -0700

    added a template and directive

--- dan-stineback/app/js/cows/controllers/cow_controller.js
+++ dan-stineback/app/js/cows/controllers/cow_controller.js
@@ -3,10 +3,9 @@ const cowsay = require('cowsay-browser');
 
 module.exports = function(app) {
   app.contoller('CowController', function() {
-    const url = 'http://localhost:8080/';
     this.cow = cowsay.say({
       text: 'moooo'
     });
-    
+
   });
 };
--- dan-stineback/app/js/cows/directives/cow_directive.js
+++ dan-stineback/app/js/cows/directives/cow_directive.js
@@ -1,1 +1,13 @@
 'use strict';
+module.exports = function(app) {
+  app.directive('cowForm', function() {
+    return {
+      scope: {
+        type: '@',
+        cow: '='
+      },
+      templateUrl: './templates/cow_template.html'
+
+    };
+  });
+};
